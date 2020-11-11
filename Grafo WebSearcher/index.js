0ac3cd4bf95d4df798894f58d0c68ee5890b6fee
MurylloEx <muryllopimenta@gmail.com>
2020-11-11 02:42:32 -0300

    Correção de caracteres de escape

--- Grafo WebSearcher/index.js
+++ Grafo WebSearcher/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const queries = require('./queries');
 const Middlewares = require('./middlewares');
+const decode = require('unescape');
+const Entities = require('html-entities').AllHtmlEntities;
 
 const app = express();
 
@@ -10,11 +12,28 @@ app.set('views', path.resolve(__dirname, 'views'));
 
 app.use(Middlewares.PreOpMiddlewares);
 
+function UnescapeSpecialChars(str) {
+  const SPECIAL_CHARSET = new RegExp(/&#\d{2,6};/ig);
+  const FOUND_CHARS = String(str || '').match(SPECIAL_CHARSET) || [];
+  let newStr = String(str || '');
+  for (let idx = 0; idx < FOUND_CHARS.length; idx++) {
+    newStr = newStr.replace(FOUND_CHARS[idx], `${String.fromCharCode(Number(FOUND_CHARS[idx].replace(/[^0-9]/ig, '')))}`);
+  }
+  return Entities.decode(decode(newStr, 'all'));
+}
+
+
 app.get('/', async (req, res) => {
   if (req.query.q && req.query.page){
     if (isNaN(Number(req.query.page)))
       return res.render('search', {});
     let results = await queries.query_search_websites(req.query.q, req.query.page);
+    if (results !== false){
+      for (let k = 0; k < results.length; k++){
+        results[k].site_title = UnescapeSpecialChars(results[k].site_title);
+        results[k].site_description = UnescapeSpecialChars(results[k].site_description);
+      }
+    }
     res.render('results', {
       HasResults: (results !== false),
       Results: results,
@@ -31,7 +50,7 @@ app.get('/howitworks', (req, res) => {
 });
 
 app.get('/algorithm', (req, res) => {
-  res.render('howitworks');
+  res.render('/');
 });
 
 app.get('/webcrawler', (req, res) => {
